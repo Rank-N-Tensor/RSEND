89df8ff15f8613cb706cc95aa2120b7a02a5708a
Rank-N-Tensor <aditiw0906@gmail.com>
2022-03-29 02:20:55 +0530

    fixed build directory error

---
+++ script/world_intro.cpp
@@ -0,0 +1,17 @@
+#include<gazebo-11/gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Adi's world\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
